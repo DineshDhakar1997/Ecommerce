e312cfcb07e596d16ecf88190238451fab1ac6f2
DineshDhakar1997 <dhakardinesh1997@gmail.com>
2023-12-28 00:27:45 +0530

    updated on 28-12-2023 00:27:45

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import os
 from datetime import datetime
+os.system("cd /Users/dinesh/Desktop/MERN project")
 current_datetime = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 os.system("git add .")
 os.system(f"git commit -m 'updated on {current_datetime}'")
+os.system("git push origin main")
